470778857a72996c841e640eb846299cb2cd04f3
defeef <97907305+defeef@users.noreply.github.com>
2022-09-05 15:23:54 -0400

    snake head movement

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import time
 width, height = 10, 10
 screen = [[" " for _ in range(width)] for _ in range(height)]
 
-snake = [math.floor(width), math.floor(height)]
+snake = [[math.floor(width), math.floor(height)]]
 snake_dir = 0 # 0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT
 
 while True:
-  
+  if snake_dir == 0:
+    snake[0][0] -= 1
+  elif snake_dir == 1:
+    snake[0][1] += 1
+  elif snake_dir == 2:
+    snake[0][0] += 1
+  elif snake_dir == 3:
+    snake[0][1] -= 1
 
   time.sleep(0.05)
