bc04321e0b5bb0bf0f9bb9ee131c9ed2ab5813b1
defeef <97907305+defeef@users.noreply.github.com>
2022-09-05 15:21:02 -0400

    game loop setup

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import math
+import time
 
 width, height = 10, 10
 screen = [[" " for _ in range(width)] for _ in range(height)]
 
 snake = [math.floor(width), math.floor(height)]
+snake_dir = 0 # 0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT
+
+while True:
+  
+
+  time.sleep(0.05)
